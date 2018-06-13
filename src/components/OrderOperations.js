93ba4e6a6f059168c180aa30e5135d96c2bf23d5
Mark Andres <mandres12@gmail.com>
2018-06-13 16:28:41 -0500

    Displaying message for all unimplemented buttons.

--- src/components/OrderOperations.js
+++ src/components/OrderOperations.js
@@ -11,42 +11,63 @@ class OrderOperations extends React.Component {
     this.props.restoreTopMenu();
   };
 
-  onNoSaleClicked = e => {
-    this.props.loadModal(MESSAGE_MODAL, { message: 'No Sale has not been implemented.'});
+  onNotImplementedClicked = e => {
+    this.props.loadModal(MESSAGE_MODAL, { message: `Button '${e.target.innerText}' has not been implemented.`});
   }
 
   render() {
     return (
       <div className="main-grid-cell order-ops">
-        <div className="std-button grid-item-row1-col1">
+        <div 
+          className="std-button grid-item-row1-col1"
+          onClick={this.onNotImplementedClicked}
+        >
           <p className="std-button-caption caption-color-red large-caption">Logoff</p>
         </div>
         <div 
           className="std-button grid-item-row1-col2"
-          onClick={this.onNoSaleClicked}
+          onClick={this.onNotImplementedClicked}
         >
           <p className="std-button-caption caption-color-green large-caption">No Sale</p>
         </div>
         <div className="button-stack grid-item-row1-col3">
-          <div className="std-button grid-item-row1-col1">
+          <div 
+            className="std-button grid-item-row1-col1"
+            onClick={this.onNotImplementedClicked}
+          >
             <p className="std-button-caption medium-caption">{' '}Order Lookup</p>
           </div>
-          <div className="std-button grid-item-row2-col1">
+          <div 
+            className="std-button grid-item-row2-col1"
+            onClick={this.onNotImplementedClicked}
+          >
             <p className="std-button-caption medium-caption">Print</p>
           </div>
         </div>
         <div className="button-stack grid-item-row1-col4">
-          <div className="std-button grid-item-row1-col1">
+          <div 
+            className="std-button grid-item-row1-col1"
+            onClick={this.onNotImplementedClicked}
+          >
             <p className="std-button-caption medium-caption">Defer Order</p>
           </div>
-          <div className="std-button grid-item-row2-col1">
+          <div 
+            className="std-button grid-item-row2-col1"
+            onClick={this.onNotImplementedClicked}
+          >
             <p className="std-button-caption medium-caption caption-color-red">Exit</p>
           </div>
         </div>
-        <div className="std-button grid-item-row1-col5">
+        <div 
+          className="std-button grid-item-row1-col5"
+          onClick={this.onNotImplementedClicked}
+        >
           <p className="std-button-caption caption-color-gold large-caption">Send</p>
         </div>
-        <div className="std-button grid-item-row1-col6">
+        <div 
+          className="std-button grid-item-row1-col6"
+          onClick={this.onNotImplementedClicked}
+        >
           <p className="std-button-caption caption-color-green large-caption">Collect</p>
         </div>
       </div>
