a95b3da7d62343e58f26c5caba88389117d31642
Mark Andres <mandres12@gmail.com>
2018-06-13 20:04:09 -0500

    Created number pad for quantity dialog.

--- src/components/QuantityDialog.js
+++ src/components/QuantityDialog.js
@@ -12,6 +12,39 @@ const QuantityDiv = styled.div`
   padding: 4vw;
 `;
 
+const DialogHeading = styled.h1`
+  margin-bottom: 2vh;
+`;
+
+const NumberDisplayArea = styled.div`
+  display: table-cell;
+  text-align: right;
+  vertical-align: center;
+  border: 1px solid black;
+  height: 6vh;
+  width: 20.1vw;
+  padding-top: 3px;
+  padding-right: 3px;
+  font-size: 1.5em;
+`;
+
+const NumberInputButtons = styled.div`
+  border: 1px solid black;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+`;
+
+const NumberButton = styled.button`
+  height: 6vh; 
+  font-size: 1.5em;
+`;
+
+const DeleteButton = styled.button`
+  height: 6vh; 
+  font-size: 1.3em;
+`;
+
 const OkButton = styled.button`
   width: 8vw;
   height: 4vh;
@@ -27,7 +60,7 @@ class QuantityDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quantityStr: '1'
+      quantityStr: ''
     };
   }
 
@@ -45,23 +78,43 @@ class QuantityDialog extends React.Component {
     this.props.hideModal();
   }
 
-  onChange = e => {
+  onNumberButtonClicked = e => {
     e.preventDefault();
-    this.setState({ quantityStr: e.target.value });
+
+    const digit = e.target.innerText;
+    const quantityStr = this.state.quantityStr;
+    this.setState({ quantityStr: quantityStr + digit });
+  }
+
+  removeDigit = e => {
+    e.preventDefault();
+
+    const quantityStr = this.state.quantityStr;
+    if (quantityStr.length) {
+      this.setState({ quantityStr: quantityStr.substr(0, quantityStr.length - 1) });
+    }
   }
 
   render() {
     return (
       <Modal onClose={this.onClose}>
         <QuantityDiv>
-          <fieldset>
-            <legend>Enter quantity</legend>
-            <input
-              type='number'
-              value={this.state.quantityStr}
-              onChange={this.onChange}
-            />
-          </fieldset>
+          <DialogHeading>Enter quantity:</DialogHeading>          
+          <NumberDisplayArea>{this.state.quantityStr}</NumberDisplayArea>
+          <NumberInputButtons>
+            <NumberButton onClick={this.onNumberButtonClicked}>7</NumberButton>
+            <NumberButton onClick={this.onNumberButtonClicked}>8</NumberButton>
+            <NumberButton onClick={this.onNumberButtonClicked}>9</NumberButton>
+            <NumberButton onClick={this.onNumberButtonClicked}>4</NumberButton>
+            <NumberButton onClick={this.onNumberButtonClicked}>5</NumberButton>
+            <NumberButton onClick={this.onNumberButtonClicked}>6</NumberButton>
+            <NumberButton onClick={this.onNumberButtonClicked}>1</NumberButton>
+            <NumberButton onClick={this.onNumberButtonClicked}>2</NumberButton>
+            <NumberButton onClick={this.onNumberButtonClicked}>3</NumberButton>
+            <NumberButton onClick={this.onNumberButtonClicked}>0</NumberButton>
+            <NumberButton disabled>.</NumberButton>
+            <DeleteButton onClick={this.removeDigit}>Del</DeleteButton>
+          </NumberInputButtons> 
           <OkButton onClick={this.onOkClicked}>Ok</OkButton>
           <CancelButton onClick={this.onClose}>Cancel</CancelButton>
         </QuantityDiv>
